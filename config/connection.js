15d72e09c3b7f8897804f60510eb6045ead22d39
Q118 <shelbyfish91@gmail.com>
2020-07-23 14:02:56 -0700

    created new sequelize object set up and exported

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,14 @@
 const Sequelize = require("sequelize");
 
+const sequelize = new Sequelize("sequelize_library", "root", "password", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+module.exports = sequelize;
