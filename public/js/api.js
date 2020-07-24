c0052228a73bf609b73b3bb745801315d9719e7c
Jack Meier <jackson.meier423@gmail.com>
2020-07-23 21:47:44 -0700

    created binge model

--- public/js/api.js
+++ public/js/api.js
@@ -1,26 +1,18 @@
-const unirest = require("unirest");
+const settings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=bojack&country=uk",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host":
+      "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+    "x-rapidapi-key": "57edf28d4fmsh046dee6d8c6be2fp116f03jsnf6899b32e536"
+  }
+};
 
-const req = unirest(
-  "GET",
-  "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup"
-);
-
-req.query({
-  country: "us",
-  source_id: "tt3398228",
-  source: "imdb",
-});
-
-req.headers({
-  "x-rapidapi-host":
-    "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-  "x-rapidapi-key": "eaa1d6641dmsh4c1b0d7b7ae1fb5p1b674cjsnb2498b69433e",
-  useQueryString: true,
-});
-
-req.end(function(res) {
-  if (res.error) throw new Error(res.error);
-
+$.ajax(settings).done(res => {
+  console.log(res);
   console.log(res.body.collection.name);
   console.log(res.body.collection.picture);
   console.log(res.body.collection.locations[0].display_name);
