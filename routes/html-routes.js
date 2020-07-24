0dcf1a0520b7ea215431b1d3ce442b903ebc45ee
Myhyun <mattyihyun@gmail.com>
2020-07-24 10:58:03 -0700

    removed html files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,4 +26,11 @@ module.exports = function(app) {
   app.get("/members", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+  app.get("/index", (req, res) => {
+      res.render("index", {  });
+    
+  });
 };
+
+
