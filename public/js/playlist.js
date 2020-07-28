e8aed94b82abbd28076e8aa1bbba9354c0793839
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-27 17:59:36 -0700

    finished watching button moves the title and platform

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -14,14 +14,19 @@ $(() => {
     });
   });
 
-  // $(".finishwatch").on("click", function () {
-  //     const binges_id = this.value;
-  //     $.ajax({
-  //         method: "PUT",
-  //         url: `/api/titles/${binges_id}/alreadyWatched`
-  //     }).then(() => {
-  //         // reload page to display binge in proper column
-  //         location.reload();
-  //     });
-  // });
+  $(".finishwatch").on("click", function() {
+    const binge = {
+      id: $(this).data("id"),
+      presentlyWatching: false,
+      alreadyWatched: true
+    };
+    $.ajax({
+      method: "PATCH",
+      url: "/api/titles",
+      data: binge
+    }).then(() => {
+      // reload page to display binge in proper column
+      location.reload();
+    });
+  });
 });
